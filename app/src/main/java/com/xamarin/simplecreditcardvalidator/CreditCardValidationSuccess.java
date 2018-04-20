32abffd1f08157e7971353af9404eaf4178d30a3
Administrator <admin@example.com>
2018-04-20 13:52:21 -0300

    Initial commit

---
+++ app/src/main/java/com/xamarin/simplecreditcardvalidator/CreditCardValidationSuccess.java
@@ -0,0 +1,15 @@
+package com.xamarin.simplecreditcardvalidator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class CreditCardValidationSuccess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credit_card_validation_success);
+    }
+
+}
